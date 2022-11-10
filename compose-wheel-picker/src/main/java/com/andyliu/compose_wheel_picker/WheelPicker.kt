bd0a3d94a6ca2b0bc30966cb6bb31f56297e28d3
IamCheng5 <b2468623437132@gmail.com>
2022-11-10 14:02:16 +0800

    add HorizontalWheelPicker

--- compose-wheel-picker/src/main/java/com/andyliu/compose_wheel_picker/WheelPicker.kt
+++ compose-wheel-picker/src/main/java/com/andyliu/compose_wheel_picker/WheelPicker.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -60,6 +62,58 @@ fun VerticalWheelPicker(
             Box(
                 modifier = Modifier
                     .height(itemHeight),
+                contentAlignment = Alignment.Center
+            ) {
+                content(index)
+            }
+        }
+    }
+}
+
+@Composable
+fun HorizontalWheelPicker(
+    modifier: Modifier = Modifier,
+    count: Int,
+    state: LazyListState = rememberLazyListState(),
+    itemWidth: Dp,
+    visibleItemCount: Int,
+    onScrollFinish: (index: Int) -> Unit,
+    content: @Composable BoxScope.(index: Int) -> Unit
+) {
+    val itemHalfWidthToPx = with(LocalDensity.current) { itemWidth.toPx() / 2 }
+
+    val currentOnScrollFinish by rememberUpdatedState(onScrollFinish)
+
+    LaunchedEffect(state.isScrollInProgress) {
+        if (!state.isScrollInProgress && state.firstVisibleItemScrollOffset != 0) {
+            if (state.firstVisibleItemScrollOffset < itemHalfWidthToPx) {
+                state.animateScrollToItem(state.firstVisibleItemIndex)
+            } else if (state.firstVisibleItemScrollOffset > itemHalfWidthToPx) {
+                state.animateScrollToItem(state.firstVisibleItemIndex + 1)
+            }
+        }
+    }
+
+    LaunchedEffect(state) {
+        snapshotFlow { state.isScrollInProgress }
+            .filter {
+                !it && state.firstVisibleItemScrollOffset == 0
+            }
+            .drop(1)
+            .collect {
+                currentOnScrollFinish(state.firstVisibleItemIndex)
+            }
+    }
+
+    LazyRow(
+        modifier = modifier.width(itemWidth * visibleItemCount),
+        state = state,
+        contentPadding = PaddingValues(horizontal = itemWidth * (visibleItemCount / 2))
+    ) {
+        items(count = count, key = { it }) { index ->
+            Box(
+                modifier = Modifier
+                    .width(itemWidth),
                 contentAlignment = Alignment.Center
             ) {
                 content(index)
